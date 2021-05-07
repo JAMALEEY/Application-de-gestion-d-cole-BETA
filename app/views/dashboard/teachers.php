170b9f583a482662cebcee344a473eaaae624145
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-05-07 15:58:20 +0000

    Updates Conceptions

--- app/views/dashboard/teachers.php
+++ app/views/dashboard/teachers.php
@@ -1,11 +1,30 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <style>
 
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
+body{
+    min-height: 100vh;
+    background-color: #fff;
+}
 
+.navbar {
+
+}
 
 </style>
 
-
-
-
+<nav class="navbar navbar-expand d-flex flex-column align-item-start" id="slidebar">
+<a href="#" class="navbar-brand text-light mt-5">
+    <div class="display-5 font-weight-bold">THANOS</div>
+</a>
+<ul class="navbar-nav d-flex flex-column mt-5 w-100">
+    <li class="nav-item w-100">
+        <a href="#" class="nav-link text-light pl-4">HOME</a>
+    </li>
+</ul>
+</nav>
