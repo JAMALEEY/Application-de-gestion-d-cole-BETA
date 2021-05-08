4a7dcdd70766c4684b1765c7b0d1dd39688fbb91
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-05-08 21:15:24 +0000

    Update teachers.php

--- app/views/dashboard/teachers.php
+++ app/views/dashboard/teachers.php
@@ -14,7 +14,7 @@
 </style>
 
 <div class="container-fluid">
-    <div class="row min-vh-100 flex-column flex-md-row">
+    <div class="row min-vh-100">
         <aside class=" col-12 col-md-3 col-xl-2 p-0  shadow-lg" style="background-color :black">
             <nav class="navbar navbar-expand-md navbar-dark bd-dark flex-md-column flex-row align-items-conter py-2 text-center sticky-top" id="sidebar">
                 <div class="text-center p-1">
@@ -41,6 +41,9 @@
                         <li class="nav-item">
                             <h5> <a href="" class="nav-link ">Parents</a> </h5>
                         </li>
+                        <li class="nav-item">
+                            <h5> <a href="" class="nav-link ">Stats</a> </h5>
+                        </li>
                     </ul>
 
                 </div>
@@ -48,14 +51,136 @@
                 <div class="pt-5" id="">
                     <ul class="pt-5">
                         <li>
-                            <a href="" class="text-secondary">Logout</a>
-                            <img src="../public/img/logout.png" alt="">
+                            <a href="#" class="text-secondary">Logout
+                                <img src="../public/img/logout.png" alt="">
+                            </a>
+
                         </li>
                     </ul>
                 </div>
 
             </nav>
         </aside>
+        <!-- finish side bar -->
+
+
+
+
+        <div class="container col-10 pt-5">
+            <div class="table-responsive pt-5">
+                <div class="table-wrapper">
+                    <div class="table-title">
+                        <!-- LES PROPRIETES DU TQBLEAU -->
+                        <div class="row d-flex justify-content-between " style="background-color: black;">
+
+                            <div class="py-1 px-4 text-light">
+                                <h2>Teachers <b>Management</b></h2>
+                            </div>
+                            <div class="py-2 px-4">
+                                <a href="#" class="btn btn-light"> <i class="fa fa-user-plus"></i> </i> <span>Add </span></a>
+                                <a href="#" class="btn btn-dark"><i class="fa fa-file-download"></i> <span>Export to Excel</span></a>
+                            </div>
+                        </div>
+
+                    </div>
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Username</th>
+                                <th>E-mail</th>
+                                <th>Gender</th>
+                                <th>Register at</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>
+                                    <a href="#">
+                                        Michael Holz</a>
+                                </td>
+                                <td>04/10/2013</td>
+                                <td>Admin</td>
+                                <td><span class="status text-success">&bull;</span> Active</td>
+                                <td>
+                                    <a href="#" class="settings" title="Settings" data-toggle="tooltip"><i class="material-icons">&#xE8B8;</i></a>
+                                    <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE5C9;</i></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td><a href="#"> Paula Wilson</a></td>
+                                <td>05/08/2014</td>
+                                <td>Publisher</td>
+                                <td><span class="status text-success">&bull;</span> Active</td>
+                                <td>
+                                    <a href="#" class="settings" title="Settings" data-toggle="tooltip"><i class="material-icons">&#xE8B8;</i></a>
+                                    <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE5C9;</i></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td><a href="#"> Antonio Moreno</a></td>
+                                <td>11/05/2015</td>
+                                <td>Publisher</td>
+                                <td><span class="status text-danger">&bull;</span> Suspended</td>
+                                <td>
+                                    <a href="#" class="settings" title="Settings" data-toggle="tooltip"><i class="material-icons">&#xE8B8;</i></a>
+                                    <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE5C9;</i></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td><a href="#"> Mary Saveley</a></td>
+                                <td>06/09/2016</td>
+                                <td>Reviewer</td>
+                                <td><span class="status text-success">&bull;</span> Active</td>
+                                <td>
+                                    <a href="#" class="settings" title="Settings" data-toggle="tooltip"><i class="material-icons">&#xE8B8;</i></a>
+                                    <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE5C9;</i></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td><a href="#"> Martin Sommer</a></td>
+                                <td>12/08/2017</td>
+                                <td>Moderator</td>
+                                <td><span class="status text-warning">&bull;</span> Inactive</td>
+                                <td>
+                                    <a href="#" class="settings" title="Settings" data-toggle="tooltip"><i class="material-icons">&#xE8B8;</i></a>
+                                    <a href="#" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE5C9;</i></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div class="clearfix">
+                        <div class="hint-text">Showing <b>5</b> out of <b>5</b> entries</div>
+                        <ul class="pagination">
+                            <li class="page-item "><a href="#" class="page-link bg-dark text-light">Previous</a></li>
+                            <li class="page-item active"><a href="#" class="page-link bg-secondary border-dark">1</a></li>
+                            <li class="page-item"><a href="#" class="page-link text-dark">2</a></li>
+                            <li class="page-item "><a href="#" class="page-link text-dark">3</a></li>
+                            <li class="page-item"><a href="#" class="page-link text-dark">4</a></li>
+                            <li class="page-item"><a href="#" class="page-link text-dark">5</a></li>
+                            <li class="page-item"><a href="#" class="page-link bg-dark text-light">Next</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
     </div>
 
 </div>
