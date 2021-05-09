d958157c9901763e9a2adc951e800190cb08fa09
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-05-09 01:13:22 +0000

    Update Dashboard.php

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -23,23 +23,26 @@ class Dashboard extends Controller
 
 
 
-    public function creatTeacher()
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset()) {
+            public function creatTeacher()
+            {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset(($_POST['add_teacher'])))
+            {
+                
+                
             // sanitize
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
             $data = [
-                'contactname' => trim($_POST['contactname']),
+                'teacherphone' => trim($_POST['teacherphone']),
                 'phone' => trim($_POST['phone']),
-                'email' => trim($_POST['email']),
-                'contactadresse' => trim($_POST['contactadresse']),
+                'teachermatiere' => trim($_POST['teachermatiere']),
+                'teacherphone' => trim($_POST['teacherphone']),
                 'user_id' => trim($_SESSION['user_id']),
                 // 'title' => trim($_POST['']),
                 'contactname_error' => '',
                 'phone_error' => '',
-                'email_error' => '',
-                'contactadresse_error' => ''
+                'teachermatiere_error' => '',
+                'teacherphone_error' => ''
             ];
             if (empty($data['contactname'])) {
                 $data['contactname_error'] = 'Please enter Contact Name';
@@ -47,17 +50,16 @@ class Dashboard extends Controller
             if (empty($data['phone'])) {
                 $data['phone_error'] = 'Please enter phone number';
             }
-            if (empty($data['email'])) {
-                $data['email_error'] = 'Please enter email';
+            if (empty($data['teachermatiere'])) {
+                $data['teachermatiere_error'] = 'Please enter la matiere du professeur';
             }
-            if (empty($data['contactadresse'])) {
-                $data['contactadresse_error'] = 'Please enter adresse';
+            if (empty($data['teacherphone'])) {
+                $data['teacherphone_error'] = 'Please enter adresse';
             }
             // make sure theres no errors
-            if ((empty($data['contactname_error'])) && (empty($data['contactadresse_error'])) && (empty($data['email_error'])) && (empty($data['phone_error']))) {
+            if ((empty($data['contactname_error'])) && (empty($data['teacherphone_error'])) && (empty($data['teachermatiere_error'])) && (empty($data['phone_error']))) {
                 // validated stuff
                 if ($this->contactModel->creatContact($data)) {
-                    flash('contact_message', 'Contact Added Succefully');
                     redirect('contacts');
                 } else {
                     die('ERROR');
