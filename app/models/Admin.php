ee941d289e61731ad7241e9266a722145b5274e7
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-05-09 13:49:51 +0000

    Unrecognition of password bug

--- app/models/Admin.php
+++ app/models/Admin.php
@@ -20,17 +20,8 @@
         // log in the user 
         // go for email admin login
         public function logIn($email, $password) {
-            $this->db->query('SELECT * FROM admins WHERE Email = :email');
+            $this->db->query('SELECT * FROM admins WHERE Email = :email AND Password = :password');
             $this->db->bind(':email', $email);
-
-            $row = $this->db->single();
-            $hashed_password = $row->password;
-            if (password_verify($password, $hashed_password)) {
-                return $row;
-            }else {
-                return false;
-            }
-
         }
 
 
