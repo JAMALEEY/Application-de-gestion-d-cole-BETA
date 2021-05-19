8487d8dc20b8c0583bc4f2d49af420d59c62c3e0
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-05-19 18:18:19 +0100

    Add student.php model

---
+++ app/models/Student.php
@@ -0,0 +1,22 @@
+
+<?php
+class Teacher
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function getTeacher()
+    {
+        $this->db->query('SELECT `id`, `teachername`, `teachergender`, `teacherclasse`, `teachermatiere`, `teacherphone` FROM `teachers`');
+        $this->db->bind(':teachername', 'teachername');
+        $this->db->bind(':teachergender', 'teachergender');
+        $this->db->bind(':teacherclasse', 'teacherclasse');
+        $this->db->bind(':teachermatiere', 'teachermatiere');
+        $this->db->bind(':teacherphone', 'teacherphone');
+        $results = $this->db->resultSet();
+        return $results;
+    }
