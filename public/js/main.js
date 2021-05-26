b43502d31cd28051b3778be906d9e0aa7074dbd1
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-05-26 23:12:20 +0100

    Finish Add Teacher Form Validation

--- public/js/main.js
+++ public/js/main.js
@@ -29,6 +29,50 @@ function test(){
   });
 }
 
+
+
+    function submitContactForm() {
+
+        var reg = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+        var teachername = $('#teachername').val();
+        var teachergender = $('#teachergender').val();
+        var teacherclasse = $('#teacherclasse').val();
+        var teachermatiere = $('#teachermatiere').val();
+        var teacherphone = $('#teacherphone').val();
+
+            if ( (teachername.trim() == '') && (teachergender.trim() == '') && (teacherclasse.trim() == '') && (teachermatiere.trim() == '') && (teacherphone.trim() == '')) {
+
+            $('#teachername').focus();
+            alert('⚠️ All fields are empty... Please Fill all the form fields in order to add a new teacher !');
+            return false;
+
+        } else if (teachername.trim() == '') {
+            $('#teachername').focus();
+            alert('Please enter your name.');
+            return false;
+            // alert('Please enter your name.');
+            // $('#teachername').focus();
+            // return false;
+        } else if (teachergender.trim() == '') {
+            alert('Please enter your gender.');
+
+            $('#teachergender').focus();
+            return false;
+        } else if (teacherclasse.trim() == '') {
+            alert('Please enter your class.');
+            $('#teacherclasse').focus();
+            return false;
+        } else if (teachermatiere.trim() == '') {
+            alert('Please enter your matiere.');
+            $('#teachermatiere').focus();
+            return false;
+        } else if (teacherphone.trim() == '') {
+            alert('Please enter your phone.');
+            $('#teacherphone').focus();
+            return false;
+        }
+    }
+
 $(document).ready(function(){
   setTimeout(function(){ test(); });
 });
@@ -52,3 +96,5 @@ $('#studentsModal').on('shown.bs.modal', function () {
 $('#parentsModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
+
+
